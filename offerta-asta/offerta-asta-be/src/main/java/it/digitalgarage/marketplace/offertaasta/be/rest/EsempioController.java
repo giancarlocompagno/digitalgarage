514a3569913250cc64d1b2a010bbed4eaa218d0f
marcus <mrandisi@gmail.com>
2018-06-20 15:45:57 +0200

    Example with GET target

--- offerta-asta/offerta-asta-be/src/main/java/it/digitalgarage/marketplace/offertaasta/be/rest/EsempioController.java
+++ offerta-asta/offerta-asta-be/src/main/java/it/digitalgarage/marketplace/offertaasta/be/rest/EsempioController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping(value="/esempio")
@@ -36,7 +37,20 @@ public class EsempioController {
 		}else{
 			return new ResponseEntity<>(new RispostaDTO("sono GET: utente non creato"),HttpStatus.BAD_REQUEST);
 		}
+	}
+	
+	
+	@ResponseBody
+	@RequestMapping(value="test1",method=RequestMethod.GET)
+	public ResponseEntity<RispostaDTO> saluta2(@RequestParam("nome") String nome){
+		
+		if(nome!=null){
+			return new ResponseEntity<>(new RispostaDTO("sono GET: ciao "+nome),HttpStatus.OK);
+		}else{
+			return new ResponseEntity<>(new RispostaDTO("sono GET: utente non creato"),HttpStatus.BAD_REQUEST);
+		}
 		
 	}
-
+	
 }
+
